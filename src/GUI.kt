3c2586384ab35b1a43ab9cf6c4a3edbed6c51639
Ukuthula <julian.hesse4@gmx.de>
2017-07-23 22:50:20 +0200

    Adds GUI Class and implements a test-window

--- src/GUI.kt
+++ src/GUI.kt
@@ -3,21 +3,41 @@
  */
 
 import javafx.application.Application
+import javafx.event.ActionEvent
 import javafx.scene.Scene
 import javafx.scene.control.Button
-import javafx.stage.Stage
+import javafx.scene.control.Label
+import javafx.scene.layout.HBox
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
+import javafx.stage.Stage
 
 open class GUI: Application(){
 
     override fun start(primaryStage: Stage?){
-        val btn: Button = Button("Click Me !")
+        val coin = FlipEngine()
 
-        val layoutTest: StackPane = StackPane()
-        layoutTest.children.add(btn)
+        val btn: Button = Button("Flip Coin")
+        val txt: Label = Label()
+        btn.setOnAction { event: ActionEvent? ->
+            run {
+                var spin = coin.spinCoin()
+                if ( spin == 0){
+                    txt.text = "Head"
+                }
+                else{
+                    txt.text = "Number"
+                }
+            }
+        }
 
-        val scene: Scene = Scene(layoutTest, 400.0, 400.0, Color.ORANGE)
+        val centerLayout: StackPane = StackPane()
+        val rowLayout: HBox = HBox()
+        rowLayout.children.add(btn)
+        rowLayout.children.add(txt)
+        centerLayout.children.add(rowLayout)
+
+        val scene: Scene = Scene(centerLayout, 400.0, 400.0, Color.ORANGE)
 
         primaryStage?.scene = scene
         primaryStage?.title = "Coin Flip Simulator"
